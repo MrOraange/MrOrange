c911447cc34a0c7848043e8bac9f8f2f9fe5010e
iopage <meenapraveen2009@gmail.com>
2024-02-08 14:57:02 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log('Hello World!');
+//Function for menu bar
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//Theme toggle function
+function toggleCSS() {
+    var styleLink = document.querySelector('link[href="/styles/style-light.css"]');
+
+    if (styleLink) {
+        // If the link exists, remove it
+        styleLink.remove();
+    } else {
+        // If the link doesn't exist, add it
+        styleLink = document.createElement('link');
+        styleLink.rel = 'stylesheet';
+        styleLink.type = 'text/css';
+        styleLink.href = '/styles/style-light.css';
+
+        document.head.appendChild(styleLink);
+    }
+}
+
+// Assuming you have an anchor element with the ID "toggleLink"
+var toggleLink = document.getElementById("toggleLink");
+
+// Attach the click event to the anchor
+if (toggleLink) {
+    toggleLink.addEventListener("click", toggleCSS);
+}
