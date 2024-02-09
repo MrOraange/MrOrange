f5123b29781172baecc2488cd5dd57a466290b12
iopage <meenapraveen2009@gmail.com>
2024-02-09 22:20:29 +0530

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Function for menu bar
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  var x = document.getElementById("menulink");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
