223a951453bf163e5046a2a7a973112b05a04aa6
iopage <meenapraveen2009@gmail.com>
2024-02-28 18:33:51 +0530

    update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,35 +1,45 @@
-//Function for menu bar
+// Hamburger Function
 function myFunction() {
-  var x = document.getElementById("menulink");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var menu = document.getElementsByClassName("topmenu")[0];
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
   } else {
-    x.style.display = "block";
+    menu.style.display = "flex";
   }
 }
 
-//Theme toggle function
+// Theme toggle function
 function toggleCSS() {
-    var styleLink = document.querySelector('link[href="/styles/style-dark.css"]');
+  var styleLink = document.querySelector('link[href="/styles/style-dark.css"]');
 
-    if (styleLink) {
-        // If the link exists, remove it
-        styleLink.remove();
-    } else {
-        // If the link doesn't exist, add it
-        styleLink = document.createElement('link');
-        styleLink.rel = 'stylesheet';
-        styleLink.type = 'text/css';
-        styleLink.href = '/styles/style-dark.css';
+  if (styleLink) {styleLink.remove();}
+  else {
+    styleLink = document.createElement('link');
+    styleLink.rel = 'stylesheet';
+    styleLink.type = 'text/css';
+    styleLink.href = '/styles/style-dark.css';
 
-        document.head.appendChild(styleLink);
-    }
+    document.head.appendChild(styleLink);
+  }
 }
-
-// Assuming you have an anchor element with the ID "toggleLink"
 var toggleLink = document.getElementById("toggleLink");
-
-// Attach the click event to the anchor
 if (toggleLink) {
-    toggleLink.addEventListener("click", toggleCSS);
+  toggleLink.addEventListener("click", toggleCSS);
 }
+
+//Function scroll
+function handleScroll(elementClass, scrollThreshold) {
+    var menu = document.querySelector('.' + elementClass);
+
+    window.addEventListener('scroll', function() {
+        if (document.documentElement.scrollTop > scrollThreshold) {
+            menu.classList.add(elementClass + '-scroll');
+        } else {
+            menu.classList.remove(elementClass + '-scroll');
+        }
+    });
+}
+// Usage
+handleScroll('topbar', 200);
+handleScroll('topline', 200);
+handleScroll('topmenu', 100); 
