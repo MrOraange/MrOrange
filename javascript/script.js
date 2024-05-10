e6125801a6610658c279399acbd82a9c378a8ed7
iopage <meenapraveen2009@gmail.com>
2024-05-10 18:43:03 +0530

    update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,6 @@ function ham() {
   let nav = document.querySelector('.navigations');
   let menu = document.querySelector('.ri-menu-line');
   let close = document.querySelector('.ri-close-line');
-  
   if (nav.style.display === 'flex') {
     nav.style.display = 'none';
     close.classList.add('ri-menu-line');
@@ -14,21 +13,23 @@ function ham() {
     menu.classList.remove('ri-menu-line');
   }
 }
-
 let darkAdded, head, link;
 function theme() {
   if (!darkAdded) {
+    localStorage.setItem('theme', "dark");
     head = document.head;
     link = document.createElement("link"); 
-
     link.type = 'text/css';
     link.rel = 'stylesheet';
     link.href = '/css/style-dark.css';
-
     head.appendChild(link);
     darkAdded = true;
   } else {
+    localStorage.setItem('theme', 'light');
     head.removeChild(link);
     darkAdded = false;
   }
+}
+if (localStorage.getItem('theme') === 'dark') {
+  theme();
 }
